47e104c729510a458100cca1aed3b13a0abcbaf0
GabrielGuimaraes12 <100451778+GabrielGuimaraes12@users.noreply.github.com>
2023-05-29 13:22:07 -0300

    Initial commit

---
+++ FamiliaDatabase.py
@@ -0,0 +1,21 @@
+class FamiliaDatabase:
+    def __init__(self, database):
+        self.db = database
+
+    # Quem da família é Empresario?
+    def get_empresarios(self):
+        query = "MATCH (p:Pessoa:Empresario) RETURN p.nome AS nome_empresario"
+        results = self.db.execute_query(query)
+        return [result["nome_empresario"] for result in results]
+
+    # Quem é pai de Gabriel Guimaraes?
+    def get_pai_de_gabriel(self):
+        query = "MATCH (p1:Pessoa)-[:PAI_DE]->(p2:Pessoa {nome: 'Gabriel Guimaraes'}) RETURN p1.nome AS nome_pai"
+        results = self.db.execute_query(query)
+        return [result["nome_pai"] for result in results]
+
+    # Com quem Lise é casada?
+    def get_casados_com_lise(self):
+        query = "MATCH (p1:Pessoa {nome: 'Lise'})-[:CASADO_COM]->(p2:Pessoa) RETURN p2.nome AS nome_parceiro"
+        results = self.db.execute_query(query)
+        return [result["nome_parceiro"] for result in results]
---
+++ main.py
@@ -0,0 +1,25 @@
+from neo4j import GraphDatabase
+from database import Database
+from FamiliaDatabase import FamiliaDatabase
+# Configuração da conexão com o Neo4j
+db=Database("bolt://18.206.64.77:7687" , "neo4j", "checkouts-reference-dynamometers")
+db.drop_all()
+
+familia=FamiliaDatabase(db)
+
+# Consulta: Quem da família é Empresario?
+empresarios = familia.get_empresarios()
+print("Empresários da família:")
+print(empresarios)
+
+# Consulta: Quem é pai de Gabriel Guimaraes?
+pais_de_gabriel = familia.get_pai_de_gabriel()
+print("Pais de Gabriel Guimaraes:")
+print(pais_de_gabriel)
+
+# Consulta: Com quem Lise é casada?
+parceiros_de_lise = familia.get_casados_com_lise()
+print("Parceiros de Lise:")
+print(parceiros_de_lise)
+
+db.close()
